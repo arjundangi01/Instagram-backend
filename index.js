5536c833574dd9ebdecab66192000336227aaac1
arjundangi01 <arjundangi907473@gmail.com>
2023-10-25 18:56:35 +0530

    completed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const cors = require("cors")
-
+const cors = require("cors");
 const connection = require("./config/db");
 const userRouter = require("./routes/user.routes");
 const postRouter = require("./routes/post.routes");
@@ -20,16 +19,40 @@ app.use(
   })
 );
 app.use(express.json());
-app.use('/users',userRouter)
-app.use('/posts',postRouter)
-app.use('/comments',commentRouter)
-app.use('/notifications',notificationRouter)
-app.use('/followers',followerRouter)
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+app.use("/notifications", notificationRouter);
+app.use("/followers", followerRouter);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+
+  function (req, res) {
+    // Successful authentication, redirect home.
+    const token = req.token;
+    res.cookie("insta_token", token, {
+      httpOnly: false,
+      sameSite: "lax",
+    });
+    res.redirect(`http://localhost:3000`);
+  }
+);
 
 app.listen(PORT, async () => {
   try {
     await connection;
     console.log(`server running on ${PORT}`);
+    console.log("database connected");
   } catch (error) {
     console.log("error while listening", error);
   }
