09eb5561482e6afe7fa1d471e1bad8a73ee8282d
Gaurav <gauravp1335@gmail.com>
2023-10-25 07:57:54 +0530

    done from my side

--- routes/follower.routes.js
+++ routes/follower.routes.js
@@ -1,6 +1,41 @@
 
 const express = require("express");
+const FollowerModel = require("../model/follower.model");
 
 const followerRouter = express.Router();
+
+followerRouter.get('/:profile_id',async (req,res)=>{
+    try {
+        const {profile_id}=req.params;
+        const follower=await FollowerModel.find({followedTo:profile_id});
+        const following=await FollowerModel.find({followedBy:profile_id});
+        res.json({Followers:follower,Following:following});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+followerRouter.post('/:profile_id',async (req,res)=>{
+    try {
+        const userId=req.userId;
+        const {profile_id}=req.params;
+        const follower=await FollowerModel.create({followedBy:userId,followedTo:profile_id});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+followerRouter.delete('/:profile_id',async (req,res)=>{
+    try {
+        const userId=req.userId;
+        const {profile_id}=req.params;
+        const follower_delete=await FollowerModel.deleteOne({followedBy:userId,followedTo:profile_id});
+        res.send({
+            message:"follower removed"
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = followerRouter
